6bf09ceba2123a5c4541939106e213c0e16caf1b
AccidentalCodes <siyayoudeen@gmail.com>
2023-06-23 09:44:35 +0530

    new

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,53 +1,65 @@
 import React from "react";
 import c1 from "../Png/c1.png";
-import c2 from "../Png/c2.png";
-import c3 from "../Png/c3.png";
+import c4 from "../Png/c4.png";
 
 const Landing = () => {
   return (
     <>
-      <div class="grid grid-cols-12 bg-black relative ">
-        <div class="col-span-12 sm:col-span-3 absolute row-span-6 z-10">
-          <div
-            class=" text-1xl sm:text-6xl flex flex-col pl-10 pt-20 sm:pt-80 font-semibold font-['Comfortaa'] dark:text-white"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-          >
-            Use color in your
-            <span>visual storytelling</span>
+      <div>
+        <div class="grid grid-cols-12 bg-black relative ">
+          <div class="col-span-12 sm:col-span-3 absolute row-span-6 z-10">
+            <div
+              class=" text-1xl md:text-6xl flex flex-col pl-10 pt-20 sm:pt-80 font-semibold font-['Comfortaa'] dark:text-white"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
+              Use color in your
+              <span>visual storytelling</span>
+            </div>
           </div>
-        </div>
-        <div class="col-span-12 sm:col-span-12 row-span-6 ">
-          <div className="relative ">
-            <div className="absolute bg-black">
-              <img
-                src={c1}
-                alt="Logo"
-                data-aos="fade-up"
-                data-aos-duration="1000"
-              />
-            </div>
-            <div className="absolute">
-              <img
-                src={c2}
-                alt="Logo"
-                data-aos="fade-down"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-out"
-              />
-            </div>
-            <div className="absolute">
-              <img
-                src={c3}
-                alt="Logo"
-                data-aos="fade-down"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-out"
-              />
+          <div class="col-span-12 sm:col-span-12 row-span-6 h-screen bg-black ">
+            <div className="relative ">
+              <div className="absolute ">
+                <img
+                  src={c1}
+                  alt="Logo"
+                  data-aos="fade-up"
+                  data-aos-duration="1000"
+                />
+              </div>
+              <div className="absolute">
+                <img
+                  src={c4}
+                  alt="Logo"
+                  data-aos="fade-down"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      {/* <div className="relative flex justify-between h-screen bg-black ">
+        <div className="absolute ">
+          <img
+            src={c1}
+            alt="Logo"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          />
+        </div>
+        <div className="absolute">
+          <img
+            src={c4}
+            alt="Logo"
+            data-aos="fade-down"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+          />
+        </div>
+        <div className="flex items-center absolute text-4xl top-0 bottom-0 m-auto justify-center text-white ">Hello</div>
+      </div> */}
     </>
   );
 };
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,97 +1,21 @@
-import React from "react";
-import logo from "../Img/film.jpeg";
-import mypht from "../Img/siya.png";
-import mainposter from "../Img/dark2.jpg";
-import card1 from "../Img/money.jpeg";
-import card2 from "../Img/Joker.png";
-import Button from "@mui/material/Button";
-import Footer from "../Components/Footer";
-import { Link } from "react-router-dom";
-import Landing from "../Components/Landing";
+import React from 'react';
+import logo from '../Img/mylogo1.png';
+import mypht from '../Img/siya.png';
+import mainposter from '../Img/dark2.jpg';
+import card1 from '../Img/money.jpeg';
+import card2 from '../Img/Joker.png';
+import Button from '@mui/material/Button';
+import Footer from '../Components/Footer';
+import {Link} from 'react-router-dom';
+import Landing from '../Components/Landing';
+import c1 from '../Projects/Mogathirai/mogathirai.png';
 
 const Home = () => {
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" class="flex items-center">
-            {/* <img src={logo} class="h-10 mr-3 rounded-[10px] " alt="Logo" /> */}
-            <div className="flex flex-col ">
-              <span class="self-center text-1xl font-semibold font-['Oswald'] whitespace-nowrap dark:text-white">
-                SYED SIYAYOUDEEN
-              </span>
-            </div>
-          </a>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <nav className=" md:bg-black  mx-auto  items-center flex  justify-between">
+        <div>{/* <img src={logo} className="w-40 ml-10" alt="Logo" /> */}</div>
       </nav>
-
       <div class="grid grid-cols-12 gap-3 p-4 ">
         {/* <div class="col-span-12 sm:col-span-7  row-span-2 m-2 p-4 rounded-[30px] text-white items-center justify-center shadow-lg">
           <div class=" text-3xl sm:text-5xl flex flex-col  pt-10 font-semibold font-['Comfortaa'] dark:text-white">
@@ -117,10 +41,10 @@ const Home = () => {
           />
         </div> */}
 
-        <div className="col-span-12">
+        <div className="col-span-12  mb-10">
           <Landing />
         </div>
-        <div class="col-span-12 row-span-2 m-2 rounded-[30px]  shadow-lg">
+        {/* <div class="col-span-12 row-span-2 m-2 rounded-[30px]  shadow-lg">
           <div className="p-1 ">
             <img
               src={mainposter}
@@ -131,9 +55,10 @@ const Home = () => {
           <div className="m-3">
             <Button variant="contained">Play</Button>
           </div>
-        </div>
+        </div> */}
         {/* card */}
-        <div class="col-span-6 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
+        {/* Big card */}
+        {/* <div class="col-span-6 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
           <Link to="card_1">
             <img src={card1} className="w-full rounded-[20px]" alt="Logo" />
           </Link>
@@ -152,25 +77,67 @@ const Home = () => {
           <Link to="card_1">
             <img src={card1} className="w-full rounded-[20px]" alt="Logo" />
           </Link>
+        </div> */}
+        
+   
+        <div class="col-span-6 sm:col-span-3  row-span-2 ">
+          <Link to="card_1">
+            <img src={c1} className="w-full rounded-[15px]" alt="Logo" />
+          </Link>
         </div>
-        <div class="col-span-12 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
+        <div class="col-span-6 sm:col-span-3  row-span-2    ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[10px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
           <Link to="card_1">
             <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
           </Link>
         </div>
-        <div class="col-span-12 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
           <Link to="card_1">
             <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
           </Link>
         </div>
-        <div class="col-span-12 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
           <Link to="card_1">
             <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
           </Link>
         </div>
-        <div class="col-span-12 sm:col-span-3  row-span-2 m-2 rounded-[20px] p-1 ">
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
           <Link to="card_1">
             <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-12 sm:col-span-3  row-span-2  rounded-[20px]  ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[20px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-6 sm:col-span-3  row-span-2    ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[10px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-6 sm:col-span-3  row-span-2    ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[10px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-6 sm:col-span-3  row-span-2    ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[10px]" alt="Logo" />
+          </Link>
+        </div>
+        <div class="col-span-6 sm:col-span-3  row-span-2    ">
+          <Link to="card_1">
+            <img src={card2} className="w-full rounded-[10px]" alt="Logo" />
           </Link>
         </div>
       </div>
